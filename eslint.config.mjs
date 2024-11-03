95b32af971ca0e855dee6c3529e4ae342c108075
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2024-11-03 21:08:05 +0100

    chore: remove unused eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
     { files: ["**/*.{js,mjs,cjs,ts}"] },
     { ignores: [".node_modules/*", "dist/*", "docs/*"] },
-    { languageOptions: { globals: globals.node, sourceType: "commonjs" } },
-    pluginJs.configs.recommended,
+    { languageOptions: { sourceType: "commonjs" } },
     ...tseslint.configs.recommended,
     eslintPluginPrettierRecommended,
 ];
